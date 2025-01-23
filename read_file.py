cdf9fdc723d2eee8b43312b30b81707ae0c721f1
Alex Mannewitz <gene.mannewitz@gmail.com>
2025-01-23 13:56:12 -0700

    Added function def

--- read_file.py
+++ read_file.py
@@ -1,7 +1,14 @@
-# This file contains a function that will read in the files and parse them out into the table we want
 
 
-def read_file():
+def read_files():
+    """ This function will read in the combined data from multiple runs. It will accept
+    a list of potentially multiple tuples of the protein data (pg_matrix) and the 
+    peptide data (pr_matrix). 
+
+    Parameters:
+        file_list (list): A list of tuples of files (protein data, peptide data)
+            ex: [("report1.pg_matrix.tsv", "report1.pr_matrix.tsv"), ("report2.pg_matrix.tsv", "report2.pr_matrix.tsv")]
+    
+    """
     pass
-
 
