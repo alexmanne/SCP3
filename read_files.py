c54aed4d811d98d0403a0fd48dad321f40e4dea5
Alex Mannewitz <gene.mannewitz@gmail.com>
2025-01-30 13:30:05 -0700

    Edited fragpipe

--- read_files.py
+++ read_files.py
@@ -3,9 +3,9 @@ import numpy as np
 import os
 
 
-def diann_tables(protein_file="", peptide_file=""):
+def diann_1_9_tables(protein_file="", peptide_file=""):
     """ Process and populate pandas DataFrames with the data from 
-    the DIA-NN protein and peptide files, saving only the Protein.Names, 
+    the DIA-NN 1.9 protein and peptide files, saving only the Protein.Names, 
     Precursor.Id (for peptide), Protein.Group (for protein), and the 
     file name columns. Also rename these columns.
 
@@ -81,16 +81,19 @@ def diann_tables(protein_file="", peptide_file=""):
     return prot_abundance, pep_abundance
 
 
-def fragpipe_tables(protein_file="", peptide_file="", min_unique_peptides=1):
-    """ Process and populate pandas DataFrames with the data from 
-    the FragPipe protein and peptide files, saving only the Entry Name, 
+def fragpipe_22_tables(protein_file="", peptide_file="", 
+                       min_unique_peptides=1, use_maxlfq=False):
+    """ Process and populate pandas DataFrames with the data from the 
+    FragPipe v22.0 protein and peptide files, saving only the Entry Name, 
     Peptide Sequence (for peptide), Protein ID (for protein), and the 
-    file name columns. Also rename these columns.
+    file name Intensity columns. Also rename these columns.
 
     Parameters:
         protein_file (string): A pandas readable file with the protein data
         peptide_file (string): A pandas readable file with the peptide data
         min_unique_peptides (int): The minimum number of unique peptides
+        use_maxlfq (bool): Determines whether or not to use MaxLFQ Intensity
+            Default: False (uses "Intensity" columns without MaxLFQ)
 
     Returns:
         prot_abundance (DataFrame): pandas DataFrame with protein data
@@ -117,52 +120,66 @@ def fragpipe_tables(protein_file="", peptide_file="", min_unique_peptides=1):
     ## Create the base abundance table ##
     # Filters out proteins with "contam" or that are null
     peptide_table = peptide_table[~peptide_table['Protein'].str.contains("contam", na=False)]
-    protein_table = protein_table[~protein_table['Protein.Group'].str.contains("contam", na=False)]
+    protein_table = protein_table[~protein_table['Protein'].str.contains("contam", na=False)]
 
-    # Separate the columns of Intensity reading,  
-    # "Peptide Sequence" for peptides, "Protein.Group" for protein (these are unique), 
-    # and "Protein.Names" (used to filter by organism)
-    peptide_cols = peptide_table.filter(regex=' Intensity|Peptide Sequence|Entry Name').columns
-    protein_cols = protein_table.filter(regex="\\\\|Protein.Group|Protein.Names").columns
+    # Separate the columns of
+    # "Peptide Sequence" for peptides, 
+    # "Protein ID" for protein (these are unique), and
+    # "Entry Name" (used to filter by organism)
+    peptide_cols = peptide_table.filter(regex='Peptide Sequence|Entry Name').columns.to_list()
+    protein_cols = protein_table.filter(regex="Protein ID|Entry Name").columns.to_list()
+
+    # Include the MaxLFQ columns if requested, otherwise use the the Intensity columns
+    if use_maxlfq is True:
+        peptide_cols += (peptide_table.filter(regex=' MaxLFQ Intensity').columns.to_list())
+        protein_cols += (protein_table.filter(regex=' MaxLFQ Intensity').columns.to_list())
+    else:
+        # The regex ignores strings matching '.*MaxLFQ' NOT YET! NEEDS TO BE FIXED
+        ########################
+        peptide_cols += (peptide_table.filter(regex='(?!.*MaxLFQ).*Intensity').columns.to_list())
+        protein_cols += (protein_table.filter(regex='(?!.*MaxLFQ).*Intensity').columns.to_list())
+
+    print(peptide_cols)
+    print(protein_cols)
 
     # Create the abundance matrix
     pep_abundance = peptide_table.loc[:, peptide_cols]
     prot_abundance = protein_table.loc[:, protein_cols]
 
 
-    ## Rename the columns ##
-    # Create a list of just the full file path names
-    full_pep_run_names = peptide_cols.drop(['Precursor.Id', 'Protein.Names']).to_list()
-    full_prot_run_names = protein_cols.drop(['Protein.Group', 'Protein.Names']).to_list()
+    # ## Rename the columns ##
+    # # Create a list of just the full file path names
+    # full_pep_run_names = peptide_cols.drop(['Precursor.Id', 'Protein.Names']).to_list()
+    # full_prot_run_names = protein_cols.drop(['Protein.Group', 'Protein.Names']).to_list()
 
-    # If .raw is in the file name, strip off everything after .raw.
-    if ".raw" in full_pep_run_names[0]:
-        run_names = [name.split(".raw")[0] for name in full_prot_run_names]
-    else:
-        run_names = [os.path.splitext(name)[0] for name in full_prot_run_names]
+    # # If .raw is in the file name, strip off everything after .raw.
+    # if ".raw" in full_pep_run_names[0]:
+    #     run_names = [name.split(".raw")[0] for name in full_prot_run_names]
+    # else:
+    #     run_names = [os.path.splitext(name)[0] for name in full_prot_run_names]
 
-    # Create the renaming dictionary
-    pep_rename_dict = dict(zip(full_pep_run_names, run_names))
-    pep_rename_dict["Precursor.Id"] = "Sequence"
-    pep_rename_dict["Protein.Names"] = "Organism"
-    prot_rename_dict = dict(zip(full_prot_run_names, run_names))
-    prot_rename_dict["Protein.Group"] = "Accession"
-    prot_rename_dict["Protein.Names"] = "Organism"
+    # # Create the renaming dictionary
+    # pep_rename_dict = dict(zip(full_pep_run_names, run_names))
+    # pep_rename_dict["Precursor.Id"] = "Sequence"
+    # pep_rename_dict["Protein.Names"] = "Organism"
+    # prot_rename_dict = dict(zip(full_prot_run_names, run_names))
+    # prot_rename_dict["Protein.Group"] = "Accession"
+    # prot_rename_dict["Protein.Names"] = "Organism"
 
-    # Rename the file columns
-    pep_abundance.rename(columns=pep_rename_dict, inplace=True)
-    prot_abundance.rename(columns=prot_rename_dict, inplace=True)
+    # # Rename the file columns
+    # pep_abundance.rename(columns=pep_rename_dict, inplace=True)
+    # prot_abundance.rename(columns=prot_rename_dict, inplace=True)
 
-    # Change the organism column to be just the organism name
-    pep_abundance["Organism"] = pep_abundance["Organism"].apply(lambda strg: strg.split("_")[-1])
-    prot_abundance["Organism"] = prot_abundance["Organism"].apply(lambda strg: strg.split("_")[-1])
+    # # Change the organism column to be just the organism name
+    # pep_abundance["Organism"] = pep_abundance["Organism"].apply(lambda strg: strg.split("_")[-1])
+    # prot_abundance["Organism"] = prot_abundance["Organism"].apply(lambda strg: strg.split("_")[-1])
 
     return prot_abundance, pep_abundance
 
 
 
 def read_file(protein_file="", peptide_file="", file_id=0, 
-              processing_app='', min_unique_peptides=1):
+              processing_app='', min_unique_peptides=1, use_maxlfq=False):
     """ Process and populate pandas DataFrames with the data from 
     the protein and peptide files, saving only the Organism, 
     Sequence (for peptide), Accession (for protein), and the 
@@ -196,10 +213,11 @@ def read_file(protein_file="", peptide_file="", file_id=0,
 
     ## Create a pandas DataFrame for peptide and protein abundance ##
     if processing_app == "diann":
-        prot_abundance, pep_abundance = diann_tables(protein_file, peptide_file)
+        prot_abundance, pep_abundance = diann_1_9_tables(protein_file, peptide_file)
     
     elif processing_app == "fragpipe":
-        pass
+        prot_abundance, pep_abundance = fragpipe_22_tables(protein_file, peptide_file,
+                                                           min_unique_peptides, use_maxlfq)
 
 
     ## Rename the DataFrames with run_IDs ##
@@ -270,13 +288,16 @@ def read_files(filelist = []):
     
         ## currently set min peptides to 1. Can be changed with settings
         min_unique_peptides = 1
+        ## currently set use_maxlfq to False. Can be changed with settings
+        use_maxlfq = False
 
         # Read the file and append it to data_objects
         data_obj = read_file(protein_file=file_dict["protein_file"], 
                              peptide_file=file_dict["peptide_file"], 
                              file_id=i, 
                              processing_app= file_dict["processing_app"],
-                             min_unique_peptides=min_unique_peptides)
+                             min_unique_peptides=min_unique_peptides,
+                             use_maxlfq=use_maxlfq)
         
         data_objects.append(data_obj)
         i += 1
