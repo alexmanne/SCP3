c07fe28a6f65af78a06ca38b63dd371a133a2bb3
Alex Mannewitz <gene.mannewitz@gmail.com>
2025-02-04 16:39:23 -0700

    updated yaml incorporation

--- python_files/create_defaults.py
+++ python_files/create_defaults.py
@@ -1,10 +1,13 @@
 
 
-def generate_input_files_settings(n):
-    """ Writes a yaml file with empty values for n files 
+def generate_input_files_template(n):
+    """ Writes a yaml file with empty values for n files.
 
     Parameters:
         n (int): number of files
+
+    Raises:
+        FileExistsError: If the name "default_file_settings.yaml" already exists
     """
     comment = ("# This file creates a list of the files to import\n"
                "\n"
@@ -19,10 +22,16 @@ def generate_input_files_settings(n):
             "  protein_file: input/PUT FILE NAME HERE\n"
             "  processing_app: diann    # Options are diann or fragpipe\n")
 
-    with open("default_file_settings.yaml", "w") as file:
-        file.write(comment)
+    # Use a Try except block to signify if the file already exists
+    try:
+        with open("default_file_template.yaml", "x") as file:
+            file.write(comment)
 
-        for i in range(n):
-            file.write(f"# File {i}\n")
-            file.write(data + "\n")
+            for i in range(n):
+                file.write(f"# File {i}\n")
+                file.write(data + "\n")
+    except FileExistsError:
+        print("A file with the name 'default_file_template.yaml' already exists.\n" 
+              "Change the name of your file before generating the template.")
+        raise FileExistsError("Will not overwrite 'default_file_template.yaml'.")
 
